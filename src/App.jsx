166d03494bf5de278ed6cbcf3a0a8bb82d9e4aa8
douglas.leite <douglas.leite@fiscaltech.com.br>
2024-04-23 23:37:24 -0300

    Botão sempre presente

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import "./App.css";
 import CurrentLocationMap from "./components/CurrentLocationMap";
 
 function App() {
-  const [position, setPosition] = useState({lat: 0, lng: 0});
+  const [position, setPosition] = useState({ lat: 0, lng: 0 });
   const [deferredPrompt, setDeferredPrompt] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition(
-      (pos)=>{
+      (pos) => {
         setPosition({
           lat: pos.coords.latitude,
-          lng: pos.coords.longitude
-        })
+          lng: pos.coords.longitude,
+        });
       },
       (err) => console.log(err)
     );
 
-    window.addEventListener('beforeinstallprompt', (event) => {
+    window.addEventListener("beforeinstallprompt", (event) => {
       // Previne o comportamento padrão do browser de mostrar o prompt de instalação
       event.preventDefault();
       // Salva o evento para ser usado depois
@@ -31,10 +31,10 @@ function App() {
       deferredPrompt.prompt();
       // Aguarda o usuário responder ao prompt
       deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('Usuário aceitou a instalação');
+        if (choiceResult.outcome === "accepted") {
+          console.log("Usuário aceitou a instalação");
         } else {
-          console.log('Usuário recusou a instalação');
+          console.log("Usuário recusou a instalação");
         }
         // Limpa o prompt salvo
         setDeferredPrompt(null);
@@ -44,12 +44,8 @@ function App() {
 
   return (
     <>
-      <CurrentLocationMap
-        position={position}
-      />
-      {deferredPrompt && (
-        <button onClick={handleInstallClick}>Instalar</button>
-      )}
+      <CurrentLocationMap position={position} />
+      <button onClick={handleInstallClick}>Instalar</button>
     </>
   );
 }
